99892c28240aed3c27ebb7067bc555b9750920d8
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-19 13:05:09 -0400

    ADDED : UC6 emp wage of max working hrs

---
+++ empWagePerMonth.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+# CONSTANTS FOR THE PROGRAM
+IS_PART_TIME=1
+IS_FULL_TIME=2
+MAX_HRS_IN_MONTH=100
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
+
+#VARIIABLES
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -le $MAX_HRS_IN_MONTH && $totalWorkingDays -le $NUM_WORKING_DAYS  ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3))
+	
+	case $empCheck in
+		$IS_FULL_TIME)
+			empHrs=8;;
+		$IS_PART_TIME)
+			empHrs=4;;
+		*)
+			empHrs=0;;
+	esac
+	totalEmpHrs=$(($totalEmpHrs + $empHrs))
+done
+
+totalSalary=$(($totalEmpHrs * $EMP_RATE_PER_HR))
+
+echo "total Salary : "$totalSalary
