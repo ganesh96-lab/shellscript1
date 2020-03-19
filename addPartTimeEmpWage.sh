e93fef12adcbf456517c95e6f884741f80e6b853
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-19 08:56:30 -0400

    ADDED : UC3 add part time Emp & wage

---
+++ addPartTimeEmpWage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+#CONSTANT
+
+ISPARTTIME=1
+ISFULLTIME=2
+EMPRATEPERHRS=20
+
+randomCheck=$((RANDOM%2+1))
+echo $randomCheck
+
+if [ $ISFULLTIME -eq  $randomCheck ]
+then
+   empHrs=8
+elif [ $ISPARTTIME -eq $randomCheck ]
+then
+   empHrs=4
+fi
+
+salary=$(( $empHrs * $EMPRATEPERHRS ))
+echo $salary
+
+
