14426e14361f51e6c08b444a6c24d157d5dd5149
ganesh96-lab <54699093+ganesh96-lab@users.noreply.github.com>
2020-03-20 04:22:33 -0400

    Update addPartTimeEmpWage.sh

--- addPartTimeEmpWage.sh
+++ addPartTimeEmpWage.sh
@@ -11,23 +11,16 @@ echo $randomCheck
 
 if [ $ISFULLTIME -eq  $randomCheck ]
 then
-<<<<<<< HEAD
+
    empHrs=8
 elif [ $ISPARTTIME -eq $randomCheck ]
 then
    empHrs=4
-=======
 	empHrs=8
 elif [ $ISPARTTIME -eq $randomCheck ]
 then
 	empHrs=4
->>>>>>> UC2
 fi
 
 salary=$(( $empHrs * $EMPRATEPERHRS ))
 echo $salary
-<<<<<<< HEAD
-
-
-=======
->>>>>>> UC2
